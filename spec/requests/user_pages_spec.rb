61670bc1be874818001eeca3c616e0bc396eaff4
kevdashdev <kevdashdev@gmail.com>
2013-06-09 14:52:04 +0800

    Finish user signup feature

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject { page }
+  let(:user) { FactoryGirl.create :user }
 
   describe 'profile page' do
-    let(:user) { FactoryGirl.create :user }
     before { visit user_path user }
 
     it { should have_selector 'h1', text: user.name }
@@ -16,4 +16,30 @@ describe "UserPages" do
     it { should have_selector 'h1', text: 'Sign up' }
     it { should have_selector 'title', text: full_title('Sign up') }
   end
+
+  describe 'Sign up' do
+    before { visit signup_path }
+    let(:submit) { 'Create my account' }
+
+    describe 'with invalid information' do
+      it 'should not create a user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'with valid information' do
+      before do
+        fill_in 'Name',         with: 'Kevin Candelaria'
+        fill_in 'Email',        with: 'kevdashdev@gmail.com'
+        fill_in 'Password',     with: 'password'
+        fill_in 'Confirmation', with: 'password'
+      end
+
+      it 'should createa a user' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
+  end
+
 end
