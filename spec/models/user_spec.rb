79c9fd3b3cb002f7e634b57b97e8cba0c9eae2af
kevdashdev <kevdashdev@gmail.com>
2013-06-09 12:41:00 +0800

    Add validation to name and email attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,54 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new name: 'Example User', email: 'user@example.com' }
+  subject { @user }
+
+  it { should respond_to :name }
+  it { should respond_to :email }
+
+  it { should be_valid }
+
+  describe 'when name is not present' do
+    before { @user.name = ' '}
+    it { should_not be_valid }
+  end
+
+  describe 'when name is too long' do
+    before { @user.name = 'x' * 255 }
+    it { should_not be_valid }
+  end
+
+  describe 'when email is not present' do
+    before { @user.email = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'when email format is invalid' do
+    it 'should be invalid' do
+      %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com].each do |address|
+        @user.email = address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe 'when email format is valid' do
+    it 'should be valid' do
+      %w[hello@world.com ruby@rails.org wag@po.co.jp insta@gram.ch myemail+token@gmail.com].each do |address|
+        @user.email = address
+        @user.should be_valid
+      end
+    end
+  end
+
+  describe 'when email address is already taken' do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+    it { should_not be_valid }
+  end
+
 end
