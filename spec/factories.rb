ae4741bd1c4897c59faebeb0cfbe0364256895aa
kevdashdev <kevdashdev@gmail.com>
2013-06-09 14:16:50 +0800

    Add FactoryGirl and speed up tests by tweaking BCrypt on test env

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                    'Kevin Candelaria'
+    email                   'kevdashdev@gmail.com'
+    password                'password'
+    password_confirmation   'password'
+  end
+end
