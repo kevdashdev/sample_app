d11984253a6cc07e6a117dc07af32ea9bff55c9f
kevdashdev <kevdashdev@gmail.com>
2013-06-09 10:29:14 +0800

    Use RSpec shared example to eliminate test duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,33 +1,44 @@
 require 'spec_helper'
-describe "Static pages" do
-
-  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+describe 'Static pages' do
 
   subject { page }
 
-  describe "Home page" do
+  shared_examples_for 'all static pages' do
+    it { should have_selector 'h1', text: heading }
+    it { should have_selector 'title', text: full_title(page_title) }
+  end
+
+  describe 'Home page' do
     before { visit root_path }
-    it { should have_selector 'title', text: full_title('') }
-    it { should have_selector 'h1', text: 'Sample App' }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like 'all static pages'
     it { should_not have_selector 'title', text: '| Home'}
   end
 
-  describe "Help page" do
+  describe 'Help page' do
     before { visit help_path }
-    it { should have_selector 'title', text: full_title('Help') }
-    it { should have_selector 'h1', text: 'Help' }
+    let(:page_title) { 'Help' }
+    let(:heading) { 'Help' }
+
+    it_should_behave_like 'all static pages'
   end
 
   describe 'About page' do
     before { visit about_path }
-    it { should have_selector 'title', text: full_title("About") }
-    it { should have_selector 'h1', text: 'About Us' }
+    let(:page_title) { 'About' }
+    let(:heading) { 'About Us' }
+
+    it_should_behave_like 'all static pages'
   end
 
   describe 'Contact page' do
     before { visit contact_path }
-    it { should have_selector 'title', text: full_title("Contact") }
-    it { should have_selector 'h1', text: 'Contact' }
+    let(:page_title) { 'Contact' }
+    let(:heading) { 'Contact' }
+
+    it_should_behave_like 'all static pages'
   end
 
 end
