e4627c3e751c7a2423d4e59ea38725e7f845780d
kevdashdev <kevdashdev@gmail.com>
2013-06-09 22:18:22 +0800

    Finish exercises for chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   # SOURCE: http://fightingforalostcause.net/misc/2006/compare-email-regex.php
